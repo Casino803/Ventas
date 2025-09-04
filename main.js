5176bcfbc62884da5018aade02e656400c74e0b0
Casino803 <casinocomunicaciones803@gmail.com>
2025-09-04 12:52:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -562,7 +562,7 @@ function renderProductCategoriesList() {
                     console.error("Error al eliminar la categoría de producto:", error);
                     showModal("Error al eliminar la categoría de producto.");
                 }
-            }, () => {});
+            } () => {});
         });
     });
 }
